7bb44f806b7d8956480266f90585c84ac8873f31
zohaib-011 <zohaibch03756@gmail.com>
2024-09-27 18:54:47 +0500

    main

--- src/Components/ReactorMessage.js
+++ src/Components/ReactorMessage.js
@@ -5,7 +5,7 @@ return (
     <div className='container2' style={{backgroundColor:"#119683"}}>
     <div  className='panelContainer'>
 <MsnVsnItem clas='msgItem' title="Reactor's Message" width="400px" description="Welcome to Air Clean Vehicle Tech! We are dedicated to revolutionizing vehicle emissions through innovative technologies that pave the way for a cleaner, healthier future. Thank you for joining us on our journey to create sustainable solutions for a better planet."/>
-<div><img src="/images/founder.jpg" style={{height:'300px'}} alt="" />
+<div><img src="/images/zohaib.jpg" style={{height:'300px'}} alt="" />
 <p id="founderTitle">Founder</p>
 </div>
     </div>
--- src/Components/contact.js
+++ src/Components/contact.js
@@ -5,10 +5,11 @@ return (
     <div >
      
     <div className='container2' style={{paddingTop:'100px' , backgroundColor:"#119683"}}>
-      <ContactItem url="/images/founder.jpg" title="CEO" name="Aqsa Maryam" phoneNo="+92 325 6164321" email="aircleanvehicletech@gmail.com"/>  
+      <ContactItem url="/images/zohaib.jpg" title="CEO" name="Zohaib Suleman" phoneNo="+92 343 0637931" email="aircleanvehicletech@gmail.com"/>  
 
       <ContactItem url="/images/sikandar.jpg" title="Chief Marketing Officer" name="Sikandar Tariq" phoneNo="+92 318 7370673" email="chaudharysikandar022@gmail.com"/> 
       <ContactItem url="/images/tayyaba.jpg" title="Research Coordinator" name="Tayyaba Zaman" phoneNo="+92 341 4142316" email="tayyabazaman734@gmail.com"/> 
+      <ContactItem url="/images/hanzala.jpg" title="Product Manager" name="Hanzala Shakir" phoneNo="+92 355 7022417" email="hanzala0925@gmail.com"/> 
       <ContactItem url="/images/zohaib.jpg" title="Web Developer" name="Zohaib Suleman" phoneNo="+92 343 0637931" email="zohaibch03756@gmail.com"/> 
     </div>
     </div>
